0eb4f0167da1a5d44a60d2ccc49b8bc4cbdeaf25
salonikap <125595345+salonikap@users.noreply.github.com>
2024-08-29 17:06:57 +0530

    perfectly working prepaid data ready to be merged

---
+++ telezon-main/Project Main/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/telezon/src/main/java/com/telezon/dao/CustomerDao.java
@@ -0,0 +1,14 @@
+package com.telezon.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.telezon.model.Customer;
+
+import java.util.List;
+
+@Repository
+public interface CustomerDao extends JpaRepository<Customer, Integer> {
+    List<Customer> findByPrepaidPlanIsNotNull();
+    List<Customer> findByPostpaidPlanIsNotNull();
+}
---
+++ telezon-main/Project Main/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/telezon/src/test/java/com/verizon/springbootapp1/Springbootapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verizon.springbootapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
