cb44c1baebb3329e8bca0fc682c5074b10986b41
Mohammed Fawaz <127772058+fawaaaaz@users.noreply.github.com>
2024-09-03 23:15:02 +0530

    Prepaid & Postpaid exceptions added
    hopefully its working

--- Project Main/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/telezon/src/main/java/com/telezon/exception/GlobalExceptionHandler.java
+++ Project Main/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/telezon/src/main/java/com/telezon/exception/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package com.telezon.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,4 +21,12 @@ public class GlobalExceptionHandler {
     }
 
     // Other exception handlers...
+  
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = Exception.class)
+    public String handleGenericException(Exception e, Model model) {
+        model.addAttribute("errorMessage", "An unexpected error occurred: " + e.getMessage());
+        return "error";
+    }
+      
 }
--- Project Main/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/telezon/src/main/java/com/telezon/model/Postpaid.java
+++ Project Main/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/springbootapp1-PLANCRUDoperations/telezon/src/main/java/com/telezon/model/Postpaid.java
@@ -6,24 +6,36 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="postpaid")
 public class Postpaid {
 
     @Id
+    @NotNull(message = "Plan ID is required")
     private Integer planId;
 
     @Column(name="plan_name")
+    @NotNull(message = "Plan Name is required")
+    @Size(min = 2, max = 100, message = "Plan Name must be between 2 and 100 characters")
     private String planName;
 
     @Column(name="plan_price")
+    @NotNull(message = "Plan Price is required")
+    @Min(value = 0, message = "Plan Price must be greater than or equal to 0")
     private Double planPrice;
 
     @Column(name="plan_bill_cycle")
+    @NotNull(message = "Plan Bill Cycle is required")
+    @Min(value = 1, message = "Plan Bill Cycle must be greater than or equal to 1")
     private Integer planBillCycle;
 
     @Column(name="plan_data_cap")
+    @NotNull(message = "Plan Data Cap is required")
+    @Min(value = 0, message = "Plan Data Cap must be greater than or equal to 0")
     private Double planDataCap;
 
     public Postpaid() {}
